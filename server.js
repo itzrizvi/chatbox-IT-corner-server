884a2dcf39fade7343a37a58f5474e4928e70a6a
itzrizvi <shahriar.me.work@gmail.com>
2021-12-24 00:54:15 +0600

    Server Changes

--- server.js
+++ server.js
@@ -5,41 +5,45 @@ const port = 5000 || process.env.PORT;
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer,  {
+const io = new Server(httpServer, {
     cors: {
-      origin: "*",
-      methods:["GET", "POST"],
+        origin: "*",
+        methods: ["GET", "POST"],
     },
-  });
+});
 
 
-app.get('/', (req, res)=>{
-    res.status(200).json({name:"Server"})
+app.get('/', (req, res) => {
+    res.status(200).json({ name: "Server" })
 })
 
 const users = {};
 
 io.on("connection", (socket) => {
-  console.log('Some one is Connected and Socket id' + socket.id);
+    console.log('Some one is Connected and Socket id' + socket.id);
 
-  socket.on('disconnect', ()=>{
-      console.log(`${socket.id} is Disconnected`);
+    socket.on('disconnect', () => {
+        console.log(`${socket.id} is Disconnected`);
 
-      for(let user in users){
-          if(users[user] ===socket.id){
-              delete users[user];
-          }
-      }
-  });
+        // Remove Disconnected User
+        for (let user in users) {
+            if (users[user] === socket.id) {
+                delete users[user];
+            }
+        }
 
-  socket.on('new_user', (userName)=>{
-      console.log(`Server: ${userName}`);
-      users[userName]= socket.id;
+        io.emit('all_users', users);
 
-      // Tell Others that someone connected
-      io.emit('new_user', users);
+    });
 
-  });
+    socket.on('new_user', (userName) => {
+        console.log(`Server: ${userName}`);
+        users[userName] = socket.id;
+
+        // Tell Others that someone connected
+        io.emit('all_users', users);
+
+    });
 });
 
 httpServer.listen(port, () => {

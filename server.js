611a6ec24a1ef453d61ebee761066c55912ff16c
itzrizvi <shahriar.me.work@gmail.com>
2021-12-27 20:11:16 +0600

    API added

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const port = 5000 || process.env.PORT;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
@@ -11,6 +17,45 @@ const io = new Server(httpServer, {
         methods: ["GET", "POST"],
     },
 });
+
+
+const uri = "mongodb+srv://chatBox:SpiderMan(NoWayHome)@cluster1.fprcc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+
+    try{
+        await client.connect();
+        const database = client.db("chatBox");
+        const userAdminsCollection = database.collection('user_Admins');
+        console.log("DB CONNECTED");
+
+        // // GET all admins
+        app.get('/admins', async (req,res)=>{
+            const cursor = userAdminsCollection.find({});
+            const allAdmins = await cursor.toArray();
+            res.send(allAdmins);
+        });
+
+        app.get('/admins/:name', async(req,res)=>{
+            const name  = req.params.name;
+            const query = {name: name}
+            const adminMatched = await userAdminsCollection.findOne(query);
+            res.json(adminMatched);
+        });
+
+    }
+    finally{
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.status(200).json({ name: "Server" })

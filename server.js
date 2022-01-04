c154191f080ea8a17adf647586cd269313e6f477
itzrizvi <shahriar.me.work@gmail.com>
2022-01-04 18:02:31 +0600

    Some Changes

--- server.js
+++ server.js
@@ -12,105 +12,97 @@ app.use(express.json());
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
+const uri =
+  "mongodb+srv://chatBox:SpiderMan(NoWayHome)@cluster1.fprcc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const uri = "mongodb+srv://chatBox:SpiderMan(NoWayHome)@cluster1.fprcc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("chatBox");
+    const userAdminsCollection = database.collection("user_Admins");
+    // const msgHistoryCollection = database.collection('twinMessege_history');
+    console.log("DB CONNECTED");
 
-async function run(){
+    // // GET all admins
+    app.get("/admins", async (req, res) => {
+      const cursor = userAdminsCollection.find({});
+      const allAdmins = await cursor.toArray();
+      res.send(allAdmins);
+    });
 
-    try{
-        await client.connect();
-        const database = client.db("chatBox");
-        const userAdminsCollection = database.collection('user_Admins');
-        // const msgHistoryCollection = database.collection('twinMessege_history');
-        console.log("DB CONNECTED");
+    // Query Admins
+    app.get("/admins/:name", async (req, res) => {
+      const name = req.params.name;
+      const query = { name: name };
+      const adminMatched = await userAdminsCollection.findOne(query);
+      res.json(adminMatched);
+    });
 
-        // // GET all admins
-        app.get('/admins', async (req,res)=>{
-            const cursor = userAdminsCollection.find({});
-            const allAdmins = await cursor.toArray();
-            res.send(allAdmins);
-        });
-
-        // Query Admins
-        app.get('/admins/:name', async(req,res)=>{
-            const name  = req.params.name;
-            const query = {name: name}
-            const adminMatched = await userAdminsCollection.findOne(query);
-            res.json(adminMatched);
-        });
-
-
-        // POST MSG HISTORY
-        // app.post('/msghistory', async (req,res)=>{
-        //     const messegeReq = req.body;
-        //     console.log(messegeReq);
-        //     const result = await msgHistoryCollection.insertOne(messegeReq);
-        //     res.json(result);
-        // });
-
-    }
-    finally{
-        // await client.close();
-    }
-
+    // POST MSG HISTORY
+    // app.post('/msghistory', async (req,res)=>{
+    //     const messegeReq = req.body;
+    //     console.log(messegeReq);
+    //     const result = await msgHistoryCollection.insertOne(messegeReq);
+    //     res.json(result);
+    // });
+  } finally {
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.status(200).json({ name: "Server" })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ name: "Server" });
+});
 
 const users = {};
 
 io.on("connection", (socket) => {
-    console.log('Some one is Connected and Socket id' + socket.id);
+  console.log("Some one is Connected and Socket id" + socket.id);
 
-    socket.on('disconnect', () => {
-        console.log(`${socket.id} is Disconnected`);
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} is Disconnected`);
 
-        // Remove Disconnected User
-        for (let user in users) {
-            if (users[user] === socket.id) {
-                delete users[user];
-            }
-        }
+    // Remove Disconnected User
+    for (let user in users) {
+      if (users[user] === socket.id) {
+        delete users[user];
+      }
+    }
 
-        io.emit('all_users', users);
+    io.emit("all_users", users);
+  });
 
-    });
+  socket.on("new_user", (userName) => {
+    console.log(`Server: ${userName}`);
+    users[userName] = socket.id;
 
-    socket.on('new_user', (userName) => {
-        console.log(`Server: ${userName}`);
-        users[userName] = socket.id;
+    // Tell Others that someone connected
+    io.emit("all_users", users);
+  });
 
-        // Tell Others that someone connected
-        io.emit('all_users', users);
-    });
+  socket.on("send_messege", (data) => {
+    console.log(data);
+    const socketId = users[data.receiver];
 
-    socket.on('send_messege', (data) => {
-        console.log(data);
-        const socketId = users[data.receiver];
+    io.to(socketId).emit("new_messege", data);
+  });
 
-        io.to(socketId).emit('new_messege', data);
-    });
-
-    socket.on('typingProcess', (typingKeys)=>{
-        socket.broadcast.emit('typingProcess', typingKeys);
-    })
+  socket.on("typingProcess", (typingKeys) => {
+    socket.broadcast.emit("typingProcess", typingKeys);
+  });
 });
 
 httpServer.listen(port, () => {
-    console.log(`Server Listening on port : ${port}`);
+  console.log(`Server Listening on port : ${port}`);
 });
